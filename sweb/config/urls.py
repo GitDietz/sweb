46b8496affea72ed7b8d60e6c71008d2dc806b32
GitDietz <thierrydna@me.com>
2020-09-14 19:34:42 +1000

    default landing set
    now handles default landing on root url

--- sweb/config/urls.py
+++ sweb/config/urls.py
@@ -1,18 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
+from .views import home
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    #path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    #path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
+    path('', home, name='home'),
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("sweb.users.urls", namespace="users")),
-    path('core/', include("lcore.urls", namespace="lcore")),
+    path('', include("lcore.urls", namespace="lcore")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
